2cf6b1b75c6a1f08dda0b78d9354063c6e9b4dc1
sand-storm-17 <sandeeepkumaar.0034@gmail.com>
2024-07-04 15:14:23 +0530

    Adding color pallete

---
+++ blog_app/lib/core/theme/app_pallete.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppPallete{
+  static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
+  static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
+  static const Color gradient2 = Color.fromRGBO(181, 190, 95, 1);
+  static const Color gradient3 = Color.fromRGBO(177, 60, 114, 1);
+  static const Color borderColor = Color.fromRGBO(51, 27, 80, 1);
+  static const Color whiteColor = Color.fromRGBO(254, 254, 255, 1);
+  static const Color greyColor = Color.fromRGBO(114, 114, 116, 1);
+  static const Color errorColor = Color.fromRGBO(167, 21, 16, 1); 
+}
